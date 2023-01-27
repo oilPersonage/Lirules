3b7f35e1f94338e828fb7f577da730ba36ac7dc0
oilPersonage <avmaslennikov@inno.tech>
2023-01-27 07:25:35 +0300

    repush

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 
 if (module.hot) {
-  module.hot.accept('./Root.tsx', function() {
+  module.hot.accept('./Root.tsx', function () {
     console.log('Accepting the updated printMe module!');
-  })
+  });
 }
